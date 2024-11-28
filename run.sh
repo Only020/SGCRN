954cd57f446f2d5ed9b8df75cfe5fdbf816d24b9
Only020 <pseudoloveriu@gmail.com>
2024-11-28 17:39:53 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,41 +1,41 @@
-# # model_id=5
+
 dataset=pems08
 
-python -u train.py -d $dataset -m 0.25 
+python -m train -d $dataset -m 0.25 --emb_size 32
 
-python -u train.py -d $dataset -m 0.5  
+python -m train -d $dataset -m 0.5  --emb_size 32
 
-python -u train.py -d $dataset -m 0.75 --emb_size 32
+python -m train -d $dataset -m 0.75 --emb_size 32
 
-python -u train.py -d $dataset -m 0.9 --emb_size 8
+python -m train -d $dataset -m 0.9 --emb_size 16
 
-dataset=pems04
-
-python -u train.py -d $dataset -m 0.25 
-
-python -u train.py -d $dataset -m 0.5  
-
-python -u train.py -d $dataset -m 0.75 
-
-python -u train.py -d $dataset -m 0.9  
 
 dataset=metr-la
 
-python -u train.py -d $dataset -m 0.25 
+python -m train -d $dataset -m 0.25 
 
-python -u train.py -d $dataset -m 0.5  
+python -m train -d $dataset -m 0.5  
 
-python -u train.py -d $dataset -m 0.75 
+python -m train -d $dataset -m 0.75 --emb_size 32
 
-python -u train.py -d $dataset -m 0.9  
+python -m train -d $dataset -m 0.9  --emb_size 32
 
+dataset=pems04
+
+python -m train -d $dataset -m 0.25 
+
+python -m train -d $dataset -m 0.5  
+
+python -m train -d $dataset -m 0.75 
+
+python -m train -d $dataset -m 0.9  
 
 dataset=PEMS-BAY
 
-python -u train.py -d $dataset -m 0.25 
+python -m train -d $dataset -m 0.25 
 
-python -u train.py -d $dataset -m 0.5  
+python -m train -d $dataset -m 0.5  --emb_size 32
 
-python -u train.py -d $dataset -m 0.75 
+python -m train -d $dataset -m 0.75 
 
-python -u train.py -d $dataset -m 0.9  
+python -m train -d $dataset -m 0.9   --emb_size 32
